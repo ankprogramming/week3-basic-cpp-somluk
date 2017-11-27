1e5c15dbb7461aaef8557d5368c997aed6650289
Unknown <somluk2792@gmail.com>
2017-11-27 18:11:04 +0700

    แก้ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss."
+        "Somluk"
+         "Moungsri"
+         "nickname Bow"
+         "Sex female"
+         "Bron 4 may 1998"
+         "Hobbies play internet"
+         "Status I need to rest"<< endl;
   return 0;
 }
