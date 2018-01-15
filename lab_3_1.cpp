21069ab395320bcc2fc7422f61f86b4ceca326c6
Unknown <unknown@example.com>
2018-01-15 23:29:01 +0700

    เพิ่มข้อมูล 3_1 , 3_3

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Miss."
-        "Somluk"
-         "Moungsri"
-         "nickname Bow"
-         "Sex female"
-         "Bron 4 may 1998"
-         "Hobbies play internet"
-         "Status I need to rest"<< endl;
+  cout <<"Miss Somluk Moungsri / nickname Bow / Sex female / Bron 4 may 1998 / Hobbies play internet / Status I need to rest"
+  << endl;
   return 0;
 }
--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
-  
+  const float pi=3.14315926536;
+  float r;
+  float h;
+  float Area;
+  float Round;
+  float Cylinder;
+
+  cout<< "Please enter redius : ";
+  cin >> r;
+  Area=pi*r*r;
+  cout<< "Area of Circle : "<< Area<<endl;
+  Round=2*pi*r;
+  cout<< "Roond of Circle : "<< Round<<endl;
+
+  cout<< "Piease enter hight : ";
+  cin>> h;
+  Cylinder=pi*r*r*h;
+  cout<< "Area of Cylinder : "<< Cylinder<<endl;
   return 0;
 }
